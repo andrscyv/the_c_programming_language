372921ba6e89d5c87de6ae045109410add12fc3a
Andres Cruz <andrscyv@gmail.com>
2022-10-14 22:53:23 -0500

    feat: exercise 1-14

--- 1_tutorial_introduction/charhist.c
+++ 1_tutorial_introduction/charhist.c
@@ -1,7 +1,29 @@
 // Print histogram of alphabetical chars in input
 #include <stdio.h>
+#define FREQ_TABLE_LEN 26
 
 int main()
 {
+    int freq_table[FREQ_TABLE_LEN];
+
+    for (int i = 0; i < FREQ_TABLE_LEN; i++)
+    {
+        freq_table[i] = 0;
+    }
+
+    int c;
+    while ((c = getchar()) != EOF)
+    {
+        if (c >= 'a' && c <= 'z')
+        {
+            freq_table[c - 'a']++;
+        }
+    }
+
+    for (int i = 0; i < FREQ_TABLE_LEN; i++)
+    {
+        printf("%c: %d\n", (char)('a' + i ), freq_table[i]);
+    }
     
+
 }
